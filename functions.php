2c6e4c54c2881026023657555fb4394571d896c5
Raul Escandon <soporteti@blacksheep.com.pe>
2023-12-06 10:08:50 -0500

    :hammer: Add woocommerce values

--- functions.php
+++ functions.php
@@ -22,3 +22,31 @@ function load_assets($entries)
         }
     }
 }
+
+
+/**
+ * Functions hooked into storefront_header action
+ *
+ * @hooked storefront_header_container                 - 0
+ * @hooked storefront_skip_links                       - 5
+ * @hooked storefront_social_icons                     - 10
+ * @hooked storefront_site_branding                    - 20
+ * @hooked storefront_secondary_navigation             - 30
+ * @hooked storefront_product_search                   - 40
+ * @hooked storefront_header_container_close           - 41
+ * @hooked storefront_primary_navigation_wrapper       - 42
+ * @hooked storefront_primary_navigation               - 50
+ * @hooked storefront_header_cart                      - 60
+ * @hooked storefront_primary_navigation_wrapper_close - 68
+ */
+
+add_action("bs_header", "storefront_header_container");
+add_action("bs_header", "storefront_skip_links");
+add_action("bs_header", "storefront_social_icons");
+add_action("bs_header", "storefront_secondary_navigation");
+add_action("bs_header", "storefront_product_search");
+add_action("bs_header", "storefront_header_container_close");
+add_action("bs_header", "storefront_primary_navigation_wrapper");
+add_action("bs_header", "storefront_primary_navigation");
+add_action("bs_header", "storefront_header_cart");
+add_action("bs_header", "storefront_primary_navigation_wrapper_close");
