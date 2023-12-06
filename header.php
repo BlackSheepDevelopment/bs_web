4179e659c393f4bba4c9c9cca8f8f320c692fdf8
Raul Escandon <soporteti@blacksheep.com.pe>
2023-12-06 17:18:36 -0500

    :sparkles: Add new class values

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
     <div id="page" class="hfeed site">
         <?php do_action('storefront_before_header'); ?>
 
-        <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+        <header id="masthead" class="site-header header" role="banner" style="<?php storefront_header_styles(); ?>">
 
             <?php
             /**
@@ -52,7 +52,7 @@
              * @hooked storefront_header_cart                      - 60
              * @hooked storefront_primary_navigation_wrapper_close - 68
              */
-            do_action('storefront_header');
+            // do_action('storefront_header');
             do_action('bs_header');
             ?>
 
