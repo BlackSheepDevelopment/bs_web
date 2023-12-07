6db455d9e9c81681aafafc03fa728c308d04c049
Raul Escandon <soporteti@blacksheep.com.pe>
2023-12-07 15:50:58 -0500

    Refactor header.php to include a div with class "header-container"

--- header.php
+++ header.php
@@ -36,25 +36,28 @@
 
         <header id="masthead" class="site-header header" role="banner" style="<?php storefront_header_styles(); ?>">
 
-            <?php
-            /**
-             * Functions hooked into storefront_header action
-             *
-             * @hooked storefront_header_container                 - 0
-             * @hooked storefront_skip_links                       - 5
-             * @hooked storefront_social_icons                     - 10
-             * @hooked storefront_site_branding                    - 20
-             * @hooked storefront_secondary_navigation             - 30
-             * @hooked storefront_product_search                   - 40
-             * @hooked storefront_header_container_close           - 41
-             * @hooked storefront_primary_navigation_wrapper       - 42
-             * @hooked storefront_primary_navigation               - 50
-             * @hooked storefront_header_cart                      - 60
-             * @hooked storefront_primary_navigation_wrapper_close - 68
-             */
-            // do_action('storefront_header');
-            do_action('bs_header');
-            ?>
+            <div class="col-full header-container">
+                <?php
+                /**
+                 * Functions hooked into storefront_header action
+                 *
+                 * @hooked storefront_header_container                 - 0
+                 * @hooked storefront_skip_links                       - 5
+                 * @hooked storefront_social_icons                     - 10
+                 * @hooked storefront_site_branding                    - 20
+                 * @hooked storefront_secondary_navigation             - 30
+                 * @hooked storefront_product_search                   - 40
+                 * @hooked storefront_header_container_close           - 41
+                 * @hooked storefront_primary_navigation_wrapper       - 42
+                 * @hooked storefront_primary_navigation               - 50
+                 * @hooked storefront_header_cart                      - 60
+                 * @hooked storefront_primary_navigation_wrapper_close - 68
+                 */
+                // do_action('storefront_header');
+                do_action('bs_header');
+                ?>
+            </div>
+
 
         </header><!-- #masthead -->
 
