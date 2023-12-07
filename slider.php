dd610aab37e5947be2384820c282ea5e24edcd0a
Raul Escandon <soporteti@blacksheep.com.pe>
2023-12-07 15:51:03 -0500

    Add slider component to the project

---
+++ slider.php
@@ -0,0 +1,9 @@
+<div class="slider-container">
+    <div class="slider">
+        <img class="slider__image" src="<? echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_1.jpg' ?>" alt="bs-banner-1">
+        <img class="slider__image slider__image--2" src="<? echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_2.jpg' ?>" alt=" bs-banner-2">
+        <img class="slider__image" src="<? echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_3.jpg' ?>" alt=" bs-banner-3">
+    </div>
+    <button class="prev">Prev</button>
+    <button class="next">Next</button>
+</div>
