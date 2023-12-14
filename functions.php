60503f1916778b0f253db57a9c0b473013cdf04a
Raul Escandon <soporteti@blacksheep.com.pe>
2023-12-14 18:13:40 -0500

    Add imports for store page and update home page styles

--- functions.php
+++ functions.php
@@ -24,24 +24,19 @@ function load_assets($entries)
 }
 
 
-/**
- * Functions hooked into storefront_header action
- *
- * @hooked storefront_header_container                 - 0
- * @hooked storefront_skip_links                       - 5
- * @hooked storefront_social_icons                     - 10
- * @hooked storefront_site_branding                    - 20
- * @hooked storefront_secondary_navigation             - 30
- * @hooked storefront_product_search                   - 40
- * @hooked storefront_header_container_close           - 41
- * @hooked storefront_primary_navigation_wrapper       - 42
- * @hooked storefront_primary_navigation               - 50
- * @hooked storefront_header_cart                      - 60
- * @hooked storefront_primary_navigation_wrapper_close - 68
- */
+
+add_action('bs_after_shop_loop', 'storefront_sorting_wrapper', 9);
+add_action('bs_after_shop_loop', 'woocommerce_pagination', 30);
+add_action('bs_after_shop_loop', 'storefront_sorting_wrapper_close', 31);
+
+// add_action( 'woocommerce_after_shop_loop', 'storefront_sorting_wrapper', 9 );
+// add_action( 'woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10 );
+// add_action( 'woocommerce_after_shop_loop', 'woocommerce_result_count', 20 );
+// add_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 30 );
+// add_action( 'woocommerce_after_shop_loop', 'storefront_sorting_wrapper_close', 31 );
 
 
-
+// Añadir encabezado de página de acuerdo a requerimientos (Logo, Tienda y Carrito)
 add_action("bs_header", "add_bs_logo");
 add_action("bs_header", "storefront_primary_navigation");
 add_action("bs_header", "storefront_header_cart");
--- template-fullwidth.php
+++ template-fullwidth.php
@@ -14,8 +14,8 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
         <div class="slider-container">
             <div class="slider">
-                <img class="slider__image" src="<?php echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_1.jpg' ?>" alt="bs-banner-1">
-                <img class="slider__image slider__image--2" src="<?php echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_2.jpg' ?>" alt=" bs-banner-2">
+                <img class="slider__image" src="<?php echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_2.jpg' ?>" alt="bs-banner-1">
+                <img class="slider__image slider__image--2" src="<?php echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_1.jpg' ?>" alt=" bs-banner-2">
                 <img class="slider__image" src="<?php echo get_stylesheet_directory_uri() . '/src/images/BS_BANNER_3.jpg' ?>" alt=" bs-banner-3">
             </div>
             <button class="prev">
@@ -26,9 +26,38 @@ get_header(); ?>
             </button>
         </div>
 
-        <div class="menu-categories">
-
+        <!-- Show the top 5 products from the woocommerce -->
+        <div class="col-full top-products">
+            <p class="top-products__title">Más vendidos</p>
+            <?php
+            $args = array(
+                'post_type' => 'product',
+                'posts_per_page' => 5,
+                'orderby' => 'rand'
+            );
+            $loop = new WP_Query($args);
+            if ($loop->have_posts()) {
+                echo '<ul class="products">';
+                while ($loop->have_posts()) : $loop->the_post();
+                    global $product;
+                    echo '<li class="product">';
+                    echo '<a href="' . get_permalink() . '">';
+                    echo woocommerce_get_product_thumbnail();
+                    echo '<h3>' . get_the_title() . '</h3>';
+                    echo '<span class="price">' . $product->get_price_html() . '</span>';
+                    echo '</a>';
+                    echo '</li>';
+                endwhile;
+                echo '</ul>';
+            } else {
+                echo __('No products found');
+            }
+            wp_reset_postdata();
+            ?>
         </div>
+
+
+
     </main><!-- #main -->
 </div><!-- #primary -->
 
